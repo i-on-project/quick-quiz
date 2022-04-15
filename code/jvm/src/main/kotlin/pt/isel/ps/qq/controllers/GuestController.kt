d28860c1bba022ad740a998732ac329a8f162a4d
Mexia, Vitor <Vitor.Mexia@xerox.com>
2022-04-15 20:30:54 +0100

    updating API documentation

--- code/jvm/src/main/kotlin/pt/isel/ps/qq/controllers/GuestController.kt
+++ code/jvm/src/main/kotlin/pt/isel/ps/qq/controllers/GuestController.kt
@@ -1,11 +1,15 @@
 package pt.isel.ps.qq.controllers
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import pt.isel.ps.qq.data.GiveAnswer
 import pt.isel.ps.qq.data.JoinSession
 import pt.isel.ps.qq.data.User
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.Map
 
 
 @RestController
@@ -13,8 +17,12 @@ import pt.isel.ps.qq.data.User
 class GuestController {
 
     @PostMapping("/join_session")
-    fun guestJoinSession(inputModel: JoinSession): JoinSession {
-        return inputModel
+    fun guestJoinSession(inputModel: JoinSession): Any? {
+        val uid = UUID.randomUUID().toString()
+        //var rep = ResponseEntity.BodyBuilder()
+
+
+        return null
     }
 
     @PostMapping("/answer")
--- code/jvm/src/test/kotlin/pt/isel/ps/qq/QqApplicationTests.kt
+++ code/jvm/src/test/kotlin/pt/isel/ps/qq/QqApplicationTests.kt
@@ -2,12 +2,16 @@ package pt.isel.ps.qq
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.Random
+import java.util.UUID
 
-@SpringBootTest
+//@SpringBootTest
 class QqApplicationTests {
 
 	@Test
 	fun contextLoads() {
+ 		var uid = UUID.randomUUID().toString()
+		val test = uid.hashCode()
 	}
 
 }
