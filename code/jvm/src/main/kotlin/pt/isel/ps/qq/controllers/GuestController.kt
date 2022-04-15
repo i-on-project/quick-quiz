4051b9fc455fc1d5282de1b3bfece06f9094f2fe
VitMexia <45263788+VitMexia@users.noreply.github.com>
2022-04-15 20:58:45 +0100

    fixing bad merge

--- code/jvm/src/main/kotlin/pt/isel/ps/qq/controllers/GuestController.kt
+++ code/jvm/src/main/kotlin/pt/isel/ps/qq/controllers/GuestController.kt
@@ -1,39 +1,59 @@
 package pt.isel.ps.qq.controllers
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import pt.isel.ps.qq.data.GiveAnswer
-import pt.isel.ps.qq.data.JoinSession
-import pt.isel.ps.qq.data.User
-import java.util.*
-import kotlin.collections.HashMap
-import kotlin.collections.Map
-
+import pt.isel.ps.qq.data.dto.ErrorDto
+import pt.isel.ps.qq.data.dto.UserDto
+import pt.isel.ps.qq.data.dto.UserTokenDto
+import pt.isel.ps.qq.exceptions.AlreadyExistsException
+import pt.isel.ps.qq.exceptions.InvalidTokenException
+import pt.isel.ps.qq.exceptions.UserNotFoundException
+import pt.isel.ps.qq.service.GuestService
 
 @RestController
 @RequestMapping("/api/web/v1.0/guest")
-class GuestController {
+class GuestController(
+    private val service: GuestService
+) {
 
-    @PostMapping("/join_session")
-    fun guestJoinSession(inputModel: JoinSession): Any? {
-        val uid = UUID.randomUUID().toString()
-        //var rep = ResponseEntity.BodyBuilder()
-
-
-        return null
+    companion object {
+        const val USER_DISPLAY_NAME_NOT_NULL = "displayName required"
+        const val USER_USERNAME_ALREADY_EXISTS = "username already exists"
+        const val USER_INVALID_TOKEN = "invalid token"
+        const val USER_NOT_REGISTERED = "invalid user / not registered"
     }
-
-    @PostMapping("/answer")
-    fun guestGiveAnswer(inputModel: GiveAnswer): GiveAnswer {
-        return inputModel
-    }
-
 
     @PostMapping("/register")
-    fun guestRegister(inputModel: User): User {
-        return inputModel
+    fun registerUser(@RequestBody user: UserDto): ResponseEntity<Any> {
+        if(user.displayName == null) {
+            return ResponseEntity
+                .badRequest()
+                .body(ErrorDto(code = HttpStatus.BAD_REQUEST.value(), reason = USER_DISPLAY_NAME_NOT_NULL))
+        }
+        return try {
+            ResponseEntity.ok().body(service.register(user))
+        } catch(e: AlreadyExistsException) {
+            ResponseEntity.badRequest().body(
+                ErrorDto(code = HttpStatus.BAD_REQUEST.value(), reason = USER_USERNAME_ALREADY_EXISTS)
+            )
+        }
     }
+
+    @PostMapping("/logmein")
+    fun loginUser(@RequestBody token: UserTokenDto): ResponseEntity<Any> {
+        return try {
+            ResponseEntity.ok().body(service.logmein(token))
+        } catch(e: InvalidTokenException) {
+            ResponseEntity.badRequest().body(ErrorDto(code = HttpStatus.BAD_REQUEST.value(), USER_INVALID_TOKEN))
+        } catch(e: UserNotFoundException) {
+            ResponseEntity.badRequest().body(ErrorDto(code = HttpStatus.BAD_REQUEST.value(), USER_NOT_REGISTERED))
+        }
+    }
+
+    //TODO from this point every single registered operation needs the token an username on the header
 }
 
