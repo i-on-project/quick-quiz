180556ada36d60eccb2c3e158a12e4e4a8738ab3
VitMexia <45263788+VitMexia@users.noreply.github.com>
2022-04-18 21:31:41 +0100

    unit tests~

--- code/jvm/src/test/kotlin/pt/isel/ps/qq/QqApplicationTests.kt
+++ code/jvm/src/test/kotlin/pt/isel/ps/qq/QqApplicationTests.kt
@@ -2,7 +2,6 @@ package pt.isel.ps.qq
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -42,28 +41,33 @@ class QqApplicationTests {
 	@Throws(Exception::class)
 	fun `when API-Key is missing from header validation is FORBIDEN`()  {
 		val expected = ErrorDto(HttpStatus.FORBIDDEN.value(), "Bad api-key")
-
 		mockMvc!!.post("/register") {
 			contentType = MediaType.APPLICATION_JSON
 			accept = MediaType.APPLICATION_JSON
 		}.andExpect {
 			status { isForbidden() }
-			//status { isBadRequest() }
-			//content { contentType(MediaType.APPLICATION_JSON) }
-			//content { json(mapper!!.writeValueAsString(expected)) }
+			content { json(mapper!!.writeValueAsString(expected)) }
 		}
 	}
 
 	@Test
 	@Throws(Exception::class)
 	fun `when info is missing for Register validation is BAD REQUEST`()  {
+		val expected = ErrorDto(HttpStatus.FORBIDDEN.value(), "Bad api-key")
+
+		val user = UserDto("Test")
+		println(mapper!!.writeValueAsString(user))
+
 		mockMvc!!.post("/register") {
 			contentType = MediaType.APPLICATION_JSON
 			header("API-KEY", "qwertyuiopasdfghjkzxcvbnm")
-			content = mapper!!.writeValueAsString(UserDto("test"))
-			accept = MediaType.APPLICATION_JSON
+			//accept = MediaType.APPLICATION_JSON
+			content = mapper!!.writeValueAsString(user)
+
 		}.andExpect {
 			status { isBadRequest() }
+			//content { json(mapper!!.writeValueAsString(expected)) }
+
 		}
 	}
 }
